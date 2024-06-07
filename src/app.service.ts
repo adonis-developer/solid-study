7383e8102b5f95de223b71ed4f288fd1858a2a4c
Nguyen Van Van <nvvan.0901.developer@gmail.com>
2024-06-07 14:28:19 +0700

    update jenkins file 12

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Mèo con nè 10';
+    return 'Hello World! Mèo con nè 10!!!!!';
   }
 }
